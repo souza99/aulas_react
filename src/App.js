9aec6b9d03852a981c3661420b95243cb93a5634
souza99 <jpfsouza99@gmail.com>
2022-12-13 21:54:03 -0300

    Introdução aos metodos de ciclo de vida

--- src/App.js
+++ src/App.js
@@ -5,42 +5,66 @@ import { Component } from 'react';
 class App extends Component {
 
   state = {
-    posts: []
-  }
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1',
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2',
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3',
+      }
+    ],
+  };
+  timeoutUpdate = null;
 
   // Executa quando termina de montar o component "Render()"
+  // Esse metodo é um life cicle
   componentDidMount() {
-    setTimeout(() => {
-      this.setState(
-        {
-          posts: [
-            {
-              id: 1,
-              title: 'O título 1',
-              body: 'O corpo 1',
-            },
-            {
-              id: 2,
-              title: 'O título 2',
-              body: 'O corpo 2',
-            },
-            {
-              id: 3,
-              title: 'O título 3',
-              body: 'O corpo 3',
-            }
-          ]
-        }
-      );
-    }, 5000);
+    this.handleTimeout();
   }
+
+  // É um componente que Serve para atualizar a pagina
+  // Esse metodo é um life cicle
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  // É um componente que toda vez que o component for ser desmontado, ele é chamado para desmontar,
+  // Quando acontece alguma alteração no componente, ele é chamado para desmontar "a tela por
+  // exemplo"
+  componentWillUnmount() {
+    // usando "clearTimeout" para limpar a sugeira de dados que fica na telas
+    clearTimeout(this.timeoutUpdate);
+  }
+
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+
+    posts[0].title = 'o titulo mudou';
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 1000);
+  }
+
 
   render() {
 
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
+        <h1>{counter}</h1>
         {posts.map(post => (
           <div key={post.id}>
             <h1>{post.title}</h1>
