dbfa706aab03e751c621570d2bcbac58b2445855
souza99 <jpfsouza99@gmail.com>
2022-12-19 22:40:20 -0300

    Arrumando erro de tempo de complação

--- src/App.js
+++ src/App.js
@@ -4,38 +4,30 @@ import { Component } from 'react';
 class App extends Component {
 
   state = {
-    posts: [],
+    posts: []
   };
 
-  // Executa quando termina de montar o component "Render()"
-  // Esse metodo é um life cicle
   componentDidMount() {
     this.loadPosts();
   }
 
   loadPosts = async () => {
-    //Faz a requisição para a API
     const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
 
-    const [posts] = Promise.all([postsResponse]);
+    const [posts] = await Promise.all([ postsResponse ]);
 
-    console.log(posts);
+    const postJson = await posts.json();
 
-    //converte para json o response
-    const postsJson = await posts.json();
-
-    //tena na variavel do state
-    this.setState({posts: postsJson});
-
+    this.setState({ posts: postJson });
   }
 
   render() {
     const { posts } = this.state;
 
     return (
-      <div className="App">
+      <div className="posts">
         {posts.map(post => (
-          <div key={post.id}>
+          <div key={post.id} className="post-content">
             <h1>{post.title}</h1>
             <p>{post.body}</p>
           </div>
