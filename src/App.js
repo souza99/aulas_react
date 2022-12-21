f4946bdbde5d06c59b3af1237a3363e721d2b20b
souza99 <jpfsouza99@gmail.com>
2022-12-21 00:21:04 -0300

    Concatenando ARRAYS e criando ANIMAÇÕES

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Component } from 'react';
 class App extends Component {
 
   state = {
-    posts: []
+    posts: [],
+    photos: [],
   };
 
   componentDidMount() {
@@ -14,11 +15,20 @@ class App extends Component {
   loadPosts = async () => {
     const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
 
-    const [posts] = await Promise.all([postsResponse]);
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
     const postJson = await posts.json();
+    const photoJson = await photos.json();
 
-    this.setState({ posts: postJson });
+    //FAZ UM "ZIP", une dois arrays, quando usamos, passamos o array menor primeiro
+    //para fazer o map. No caso, o postJson
+    const postsAndPhotos = postJson.map((post, index) => {
+      return { ...post, cover: photoJson[index].url }
+    });
+
+    this.setState({ posts: postsAndPhotos });
   }
 
   render() {
@@ -29,6 +39,7 @@ class App extends Component {
         <div className="posts">
           {posts.map(post => (
             <div key={post.id} className='post'>
+              <img src={post.cover} alt={post.title} />
               <div className="post-content">
                 <h1>{post.title}</h1>
                 <p>{post.body}</p>
